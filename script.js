46a952ed9f90ba2494f2cf767b2f25af4bd3a590
Jacob Antwi <135745870+Jacobantwi0771@users.noreply.github.com>
2025-03-01 14:02:50 +0000

    Uploading jake & chris architect

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu(){
+    const menuList = document.getElementById("menuList")
+    menuList.style.display='flex'
+}
+function cancelMenu(){
+    const menuList = document.getElementById("menuList")
+    menuList.style.display='none'
+}
+// const submit = document.getElementById("submit");
+// submit.onclick = function(){
+//     window.alert("Thank You For Contacting Us");
+// }
